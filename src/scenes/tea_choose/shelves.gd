b67c9dee4483651ea08c000662e2e3a0138213ed
Milana <94888u@gmail.com>
2024-06-24 01:09:34 +0200

    add make tea button

--- src/scenes/tea_choose/shelves.gd
+++ src/scenes/tea_choose/shelves.gd
@@ -1,6 +1,9 @@
 extends Control
 
 
+signal tea_completed(tea: Tea)
+
+
 @onready var base_grid := %Base as GridContainer
 @onready var flower_grid := %Flower as GridContainer
 @onready var fruit_bits_grid := %FruitBits as GridContainer
@@ -11,11 +14,21 @@ extends Control
 @onready var fruit_bits_spot := %FruitBitsSpot as Control
 
 
+@onready var finish_button := %FinishButton
+
+
 @onready var closed_book := $ClosedBook as TextureButton
 @onready var open_book := $OpenBook as Control
 
-var closed_book_initial_position := Vector2(0, 952)
+var closed_book_initial_position := Vector2(0, 824)
 var closed_book_hovered_position := Vector2(0, 568)
+
+
+func check_completed() -> void:
+	if base_spot.get_child_count() and flower_spot.get_child_count() and fruit_bits_spot.get_child_count():
+		finish_button.disabled = false
+	else:
+		finish_button.disabled = true
 
 
 func return_ingredient_back(ingredient: TeaIngredientButton) -> void:
@@ -43,6 +56,7 @@ func on_ingredient_clicked(ingredient: TeaIngredientButton) -> void:
 		return_ingredient_back(spot_to_put.get_child(0))
 	ingredient.original_spot = ingredient.get_parent()
 	ingredient.reparent(spot_to_put, false)
+	check_completed()
 
 
 func connect_buttons_from_grid(grid: GridContainer) -> void:
@@ -72,3 +86,11 @@ func _on_closed_book_mouse_entered() -> void:
 func _on_closed_book_mouse_exited() -> void:
 	var tween := closed_book.create_tween()
 	tween.tween_property(closed_book, "position", closed_book_initial_position, 0.4)
+
+
+func _on_finish_button_pressed() -> void:
+	tea_completed.emit(Tea.make_tea(
+		base_spot.get_child(0).ingredient_data,
+		flower_spot.get_child(0).ingredient_data,
+		fruit_bits_spot.get_child(0).ingredient_data,
+	))
