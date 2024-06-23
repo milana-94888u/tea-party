a9924af529d9a11cf3185d4f888a537f30f811ca
Milana <94888u@gmail.com>
2024-06-23 19:49:14 +0200

    fix dialog update

--- src/scenes/main/character.gd
+++ src/scenes/main/character.gd
@@ -1,3 +1,4 @@
+@tool
 extends Control
 
 
@@ -8,6 +9,12 @@ enum CharacterState {
 }
 
 
+@export var has_cup := false:
+	set(value):
+		has_cup = value
+		set_correct_sprites()
+
+
 @onready var back_sprite := %CharacterBack as TextureRect
 @onready var cup_spot := %CupSpot as Control
 @onready var front_sprite := %CharacterFront as TextureRect
@@ -16,7 +23,25 @@ enum CharacterState {
 @export var state := CharacterState.INITIAL
 
 
-@export var character_data: CharacterData
+func set_correct_sprites() -> void:
+	if not is_node_ready():
+		await ready
+	if not is_instance_valid(character_data):
+		back_sprite.texture = null
+		front_sprite.texture = null
+		return
+	if has_cup:
+		back_sprite.texture = character_data.back_sprite_tea
+		front_sprite.texture = character_data.front_sprite_tea
+	else:
+		back_sprite.texture = character_data.back_sprite_base
+		front_sprite.texture = character_data.front_sprite_base
+
+
+@export var character_data: CharacterData:
+	set(value):
+		character_data = value
+		set_correct_sprites()
 
 @export var introduction_dialog: Array[DialogLineData]
 @export var upset_dialog: Array[DialogLineData]
@@ -27,6 +52,7 @@ enum CharacterState {
 
 func give_cup(cup: Cup) -> void:
 	cup.reparent(cup_spot, false)
+	has_cup = true
 
 
 func evaluate_tea() -> void:
--- src/scenes/ui/dialog_line.gd
+++ src/scenes/ui/dialog_line.gd
@@ -1,11 +1,22 @@
+@tool
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var sprite := %CharacterSprite as TextureRect
+@onready var title := %Title as Label
+@onready var line_text := %LineText as Label
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@export var dialog_line_data: DialogLineData:
+	set(value):
+		dialog_line_data = value
+		if not is_node_ready():
+			await ready
+		if not is_instance_valid(dialog_line_data) or not is_instance_valid(dialog_line_data.character):
+			sprite.texture = null
+			title.text = ""
+			line_text.text = ""
+			return
+		sprite.texture = dialog_line_data.character.back_sprite_base
+		title.text = dialog_line_data.character.name
+		line_text.text = dialog_line_data.text
