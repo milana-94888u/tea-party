f2b2b6e23076bc961f85abd315cd29b3e35f8b94
Milana <94888u@gmail.com>
2024-06-24 13:03:55 +0200

    fix red tea description

--- src/scenes/learning/empty_main.gd
+++ src/scenes/learning/empty_main.gd
@@ -1,8 +1,17 @@
 extends Control
 
 
+@onready var learning_label := %LearningLabel as Label
+@onready var next_button := %NextButton as Button
+
+
+@export var info: Array[String]
+var current_info := 0
+
+
 func _ready() -> void:
-	pass
+	current_info = 0
+	learning_label.text = info[0]
 
 
 func choose_tea() -> Tea:
@@ -14,3 +23,16 @@ func choose_tea() -> Tea:
 	shelves.queue_free()
 	show()
 	return tea
+
+
+func make_tea() -> void:
+	pass
+
+
+func _on_next_button_pressed() -> void:
+	current_info += 1
+	learning_label.text = info[current_info]
+	if current_info >= len(info) - 1:
+		next_button.text = "To making tea"
+		next_button.pressed.disconnect(_on_next_button_pressed)
+		next_button.pressed.connect(make_tea)
